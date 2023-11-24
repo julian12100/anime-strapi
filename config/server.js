de653261a75b6ae84ee8fbd38fd93ce156752975
julian12100 <xfsanchez10@gmail.com>
2023-11-23 23:54:17 -0500

    Se ajustan las variables de entorno en el archivo server.js

--- config/server.js
+++ config/server.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS', []), // Aquí accedes a las claves de la aplicación
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
